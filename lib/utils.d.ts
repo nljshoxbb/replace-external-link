01fde00463e574a904df204ee034dde3bbe22de6
root <nljshoxbb@gmail.com>
2023-03-20 09:22:25 +0800

    build: remove lib

--- lib/utils.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const readFile: (file: any) => Promise<unknown>;
-export declare const writeFile: (file: any, content: any) => Promise<unknown>;
-export declare const logTask: (fileUrl: any, outputPath: any, status: "success" | "fail") => void;
-export declare function downloadFile(fileUrl: string, outputPath: string): Promise<unknown>;
